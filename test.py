de630e5c7d59fd61147c9facad4aaf431705a03e
Pavel Vietlin <justpave@gmail.com>
2022-06-15 14:37:15 +0300

    Добавляем смс пользователю и число попыток

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 import random
 
 numbers = random.randint(1,100)
-user_number = int(input())
+user_number = int(input("Enter the number: "))
+count = 0
 
 while numbers != user_number:
+    count += 1
     if numbers < user_number:
         print("Your number is greater")
     elif user_number < numbers:
         print("Your number is less")
-    user_number = int(input())
+    user_number = int(input("Enter tne number: "))
+print(f"Your try: {count}")
